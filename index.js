633f2d11ae09b15bce7216c86cb5059294f75e45
Hamdan <Hamdan.deeb91@gmail.com>
2019-12-06 17:32:40 +0100

    Created Factory Function

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: function() {
-    console.log("Hello There1");
-  }
-};
-circle.draw();
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log("Draw");
+    }
+  };
+}
+const circle = createCircle();
+circle.draw(1);
