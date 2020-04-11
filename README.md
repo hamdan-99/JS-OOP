32055fa0a5f4082681db488116b1b6621ebe397b
strik <strike.lier@hotmail.com>
2020-04-11 14:57:46 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# JS-OOP
+# languge-partner
+
+[![LoopBack](https://github.com/strongloop/loopback-next/raw/master/docs/site/imgs/branding/Powered-by-LoopBack-Badge-(blue)-@2x.png)](http://loopback.io/)
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-// Factory Function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function() {
-      console.log("Draw");
-    }
-  };
-}
-const circle = createCircle();
-circle.draw(1);
+const application = require('./dist');
 
-// constructor Function
-function Circle(radius){
-  console.log('this',this)
-  this.radius = radius,
-  this.draw = function(){
-    console.log('draw');
-  }
+module.exports = application;
+
+if (require.main === module) {
+  // Run the application
+  const config = {
+    rest: {
+      port: +(process.env.PORT || 3000),
+      host: process.env.HOST,
+      // The `gracePeriodForClose` provides a graceful close for http/https
+      // servers with keep-alive clients. The default value is `Infinity`
+      // (don't force-close). If you want to immediately destroy all sockets
+      // upon stop, set its value to `0`.
+      // See https://www.npmjs.com/package/stoppable
+      gracePeriodForClose: 5000, // 5 seconds
+      openApiSpec: {
+        // useful when used with OpenAPI-to-GraphQL to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  application.main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
 }
-const another = new Circle(1);
